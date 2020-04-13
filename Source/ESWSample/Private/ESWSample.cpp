14a7fa75b9de44dcd4ca945db08500c4bbfc5e61
sentyaanko <sentyaanko@gmail.com>
2020-04-13 15:53:07 +0900

    モジュールアンロードの後にメニューを開いた際のハングアップ対応

--- Source/ESWSample/Private/ESWSample.cpp
+++ Source/ESWSample/Private/ESWSample.cpp
@@ -13,6 +13,20 @@ static const FName ESWSampleTabName("ESWSample");
 
 #define LOCTEXT_NAMESPACE "FESWSampleModule"
 
+class FESWSampleImpl
+{
+public:
+	/** This function will be bound to Command (by default it will bring up plugin window) */
+	void PluginButtonClicked();
+	
+	void AddToolbarExtension(FToolBarBuilder& Builder);
+	void AddMenuExtension(FMenuBuilder& Builder);
+
+	TSharedRef<class SDockTab> OnSpawnPluginTab(const class FSpawnTabArgs& SpawnTabArgs);
+
+private:
+};
+
 void FESWSampleModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
@@ -23,29 +37,30 @@ void FESWSampleModule::StartupModule()
 	FESWSampleCommands::Register();
 	
 	PluginCommands = MakeShareable(new FUICommandList);
+	Impl = MakeShareable(new FESWSampleImpl);
 
 	PluginCommands->MapAction(
 		FESWSampleCommands::Get().OpenPluginWindow,
-		FExecuteAction::CreateRaw(this, &FESWSampleModule::PluginButtonClicked),
+		FExecuteAction::CreateSP(Impl.ToSharedRef(), &FESWSampleImpl::PluginButtonClicked),
 		FCanExecuteAction());
 		
 	FLevelEditorModule& LevelEditorModule = FModuleManager::LoadModuleChecked<FLevelEditorModule>("LevelEditor");
 	
 	{
 		TSharedPtr<FExtender> MenuExtender = MakeShareable(new FExtender());
-		MenuExtender->AddMenuExtension("WindowLayout", EExtensionHook::After, PluginCommands, FMenuExtensionDelegate::CreateRaw(this, &FESWSampleModule::AddMenuExtension));
+		MenuExtender->AddMenuExtension("WindowLayout", EExtensionHook::After, PluginCommands, FMenuExtensionDelegate::CreateSP(Impl.ToSharedRef(), &FESWSampleImpl::AddMenuExtension));
 
 		LevelEditorModule.GetMenuExtensibilityManager()->AddExtender(MenuExtender);
 	}
 	
 	{
 		TSharedPtr<FExtender> ToolbarExtender = MakeShareable(new FExtender);
-		ToolbarExtender->AddToolBarExtension("Settings", EExtensionHook::After, PluginCommands, FToolBarExtensionDelegate::CreateRaw(this, &FESWSampleModule::AddToolbarExtension));
+		ToolbarExtender->AddToolBarExtension("Settings", EExtensionHook::After, PluginCommands, FToolBarExtensionDelegate::CreateSP(Impl.ToSharedRef(), &FESWSampleImpl::AddToolbarExtension));
 		
 		LevelEditorModule.GetToolBarExtensibilityManager()->AddExtender(ToolbarExtender);
 	}
 	
-	FGlobalTabmanager::Get()->RegisterNomadTabSpawner(ESWSampleTabName, FOnSpawnTab::CreateRaw(this, &FESWSampleModule::OnSpawnPluginTab))
+	FGlobalTabmanager::Get()->RegisterNomadTabSpawner(ESWSampleTabName, FOnSpawnTab::CreateSP(Impl.ToSharedRef(), &FESWSampleImpl::OnSpawnPluginTab))
 		.SetDisplayName(LOCTEXT("FESWSampleTabTitle", "ESWSample"))
 		.SetMenuType(ETabSpawnerMenuType::Hidden);
 }
@@ -54,6 +69,9 @@ void FESWSampleModule::ShutdownModule()
 {
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
+
+	Impl.Reset();
+	PluginCommands.Reset();
 	FESWSampleStyle::Shutdown();
 
 	FESWSampleCommands::Unregister();
@@ -61,7 +79,7 @@ void FESWSampleModule::ShutdownModule()
 	FGlobalTabmanager::Get()->UnregisterNomadTabSpawner(ESWSampleTabName);
 }
 
-TSharedRef<SDockTab> FESWSampleModule::OnSpawnPluginTab(const FSpawnTabArgs& SpawnTabArgs)
+TSharedRef<SDockTab> FESWSampleImpl::OnSpawnPluginTab(const FSpawnTabArgs& SpawnTabArgs)
 {
 	FText WidgetText = FText::Format(
 		LOCTEXT("WindowWidgetText", "Add code to {0} in {1} to override this window's contents"),
@@ -83,17 +101,17 @@ TSharedRef<SDockTab> FESWSampleModule::OnSpawnPluginTab(const FSpawnTabArgs& Spa
 		];
 }
 
-void FESWSampleModule::PluginButtonClicked()
+void FESWSampleImpl::PluginButtonClicked()
 {
 	FGlobalTabmanager::Get()->InvokeTab(ESWSampleTabName);
 }
 
-void FESWSampleModule::AddMenuExtension(FMenuBuilder& Builder)
+void FESWSampleImpl::AddMenuExtension(FMenuBuilder& Builder)
 {
 	Builder.AddMenuEntry(FESWSampleCommands::Get().OpenPluginWindow);
 }
 
-void FESWSampleModule::AddToolbarExtension(FToolBarBuilder& Builder)
+void FESWSampleImpl::AddToolbarExtension(FToolBarBuilder& Builder)
 {
 	Builder.AddToolBarButton(FESWSampleCommands::Get().OpenPluginWindow);
 }
--- Source/ESWSample/Public/ESWSample.h
+++ Source/ESWSample/Public/ESWSample.h
@@ -16,16 +16,7 @@ public:
 	virtual void StartupModule() override;
 	virtual void ShutdownModule() override;
 	
-	/** This function will be bound to Command (by default it will bring up plugin window) */
-	void PluginButtonClicked();
-	
-private:
-
-	void AddToolbarExtension(FToolBarBuilder& Builder);
-	void AddMenuExtension(FMenuBuilder& Builder);
-
-	TSharedRef<class SDockTab> OnSpawnPluginTab(const class FSpawnTabArgs& SpawnTabArgs);
-
 private:
 	TSharedPtr<class FUICommandList> PluginCommands;
+	TSharedPtr<class FESWSampleImpl> Impl;
 };
